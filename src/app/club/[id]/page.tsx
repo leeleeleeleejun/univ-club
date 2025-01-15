478dfa029c007e5869980ccf7f6554bdf60decf8
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-15 18:41:05 +0900

    feat: 상세 페이지 generateStaticParams함수 추가 - 정적 생성으로 변경

--- src/app/club/[id]/page.tsx
+++ src/app/club/[id]/page.tsx
@@ -2,13 +2,22 @@ import ClubHeading from './_components/ClubHeading';
 import ClubIntroduction from './_components/ClubIntroduction';
 import { getClub } from './_lib';
 import LogoIcon from '@/assets/icons/logo.svg';
+import { getClubs } from '@/app/_lib';
+
+export async function generateStaticParams() {
+  const posts = await getClubs();
+
+  return posts.map((post) => ({
+    id: post.id,
+  }));
+}
 
 const ClubDetailPage = async ({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) => {
-  const id = (await params).id;
+  const { id } = await params;
   const data = await getClub(Number(id));
 
   return (
