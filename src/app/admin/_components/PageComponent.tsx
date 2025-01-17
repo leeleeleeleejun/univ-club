cd45d75f48210853c517be5c51c77e0327310b56
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-17 14:27:34 +0900

    design: admin페이지 동아리 만들기 버튼 추가

--- src/app/admin/_components/PageComponent.tsx
+++ src/app/admin/_components/PageComponent.tsx
@@ -3,6 +3,8 @@ import { useState } from 'react';
 import SearchBar from '@/app/_components/SearchBar';
 import ClubCard from '@/app/admin/_components/ClubCard';
 import { Club } from '@/types/club';
+import ActionButton from '@/app/_components/ActionButton';
+import Link from 'next/link';
 
 const PageComponent = ({ data }: { data: Club[] }) => {
   const [filterData, setFilterData] = useState(data);
@@ -23,8 +25,14 @@ const PageComponent = ({ data }: { data: Club[] }) => {
   };
   return (
     <>
-      <SearchBar handleSearch={handleSearch} />
-
+      <div className={'sticky flex items-center justify-between'}>
+        <div className={'w-full'}>
+          <SearchBar handleSearch={handleSearch} />
+        </div>
+        <Link href={'/admin/data-form'} className={'w-1/4 pr-4'}>
+          <ActionButton>만들기</ActionButton>
+        </Link>
+      </div>
       <ul className='flex flex-col gap-4 p-5 overflow-y-scroll'>
         {filterData.map((club) => (
           <ClubCard
