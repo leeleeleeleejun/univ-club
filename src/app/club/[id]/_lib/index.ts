f5eb31f36e518729ec9c22cf8429e72c062f8a5a
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-17 13:24:01 +0900

    design: ActionButton컴포넌트 위치 이동 - type 수정 (ClubFeed에서 recruitmentUrl분리) - flex 수정

--- src/app/club/[id]/_lib/index.ts
+++ src/app/club/[id]/_lib/index.ts
@@ -1,8 +1,8 @@
-import { ClubFeed, ClubInfo } from '@/types/club';
+import { ClubDetail } from '@/types/club';
 
 const BASE_URL = process.env.BASE_URL;
 
-export const getClub = async (clubId: number): Promise<ClubInfo & ClubFeed> => {
+export const getClub = async (clubId: number): Promise<ClubDetail> => {
   const response = await fetch(BASE_URL + `/api/v1/clubs/${clubId}`);
   return response.json();
 };
