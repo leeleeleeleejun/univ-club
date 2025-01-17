c1a1562a0b864fa3d97c5fc4f258845b361ee71b
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-17 16:45:27 +0900

    feat: admin club 수정 페이지 초기 데이터 가져오기

---
+++ src/app/admin/data-form/[[...id]]/_components/Input.tsx
@@ -0,0 +1,29 @@
+import { ClubDetailKey } from '@/types/club';
+
+const Input = ({
+  value,
+  placeholder = '',
+  clubDetailKey,
+  setDataFunc,
+}: {
+  value: string;
+  placeholder: string;
+  clubDetailKey: ClubDetailKey;
+  setDataFunc: (name: ClubDetailKey, data: string) => void;
+}) => {
+  return (
+    <input
+      className={commonInputStyle}
+      placeholder={placeholder}
+      value={value}
+      onChange={(event) => {
+        setDataFunc(clubDetailKey, event.target.value);
+      }}
+    />
+  );
+};
+
+export default Input;
+
+export const commonInputStyle =
+  'border rounded-md px-3 py-2 focus:ring-2 focus:ring-blue-500 outline-none';
