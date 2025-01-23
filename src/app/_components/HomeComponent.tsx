c6f3e60dbaa0e7579b63fb3b8bd100faf9ab7241
leeleeleeleejun <wnstjr6293@naver.com>
2025-01-23 18:13:51 +0900

    feat: filter관련 함수들 hook으로 분리(추상화, 응집도)

--- src/app/_components/HomeComponent.tsx
+++ src/app/_components/HomeComponent.tsx
@@ -1,83 +1,18 @@
 'use client';
-import { useState } from 'react';
+
 import SearchBar from '@/app/_components/SearchBar';
 import FilterBar from '@/app/_components/Filter';
 import ClubCard from '@/app/_components/ClubCard';
-import { Club, Filters, FilterType } from '@/types/club';
+import { Club } from '@/types/club';
+import useFilters from '@/app/_hooks/useFilters';
 
 interface HomeProps {
   initialData: Club[];
 }
 
 export default function Home({ initialData }: HomeProps) {
-  const [searchTerm, setSearchTerm] = useState<string>('');
-  const [filters, setFilters] = useState<Filters>({
-    category: [],
-    campus: '',
-    tag: [],
-  });
-
-  const handleSearch = (value: string) => {
-    setSearchTerm(value);
-  };
-
-  const handleFilter = (filterType: FilterType, value: string) => {
-    let newFilters;
-
-    if (filterType === 'campus') {
-      newFilters = {
-        ...filters,
-        campus: value,
-      };
-    } else {
-      if (value) {
-        newFilters = {
-          ...filters,
-          [filterType]: filters[filterType].includes(value)
-            ? filters[filterType].filter((item) => item !== value)
-            : [...filters[filterType], value],
-        };
-      } else {
-        newFilters = {
-          ...filters,
-          [filterType]: [],
-        };
-      }
-    }
-
-    setFilters(newFilters);
-  };
-
-  const getFilteredAndSearchedData = () => {
-    let result = initialData;
-
-    // 필터 적용
-    if (filters.category.length > 0) {
-      result = result.filter((club) =>
-        filters.category.includes(club.category)
-      );
-    }
-
-    if (filters.campus) {
-      result = result.filter((club) => club.campus === filters.campus);
-    }
-
-    if (filters.tag.length > 0) {
-      result = result.filter((club) => filters.tag.includes(club.tag));
-    }
-
-    if (searchTerm.trim()) {
-      const term = searchTerm.trim().toLowerCase();
-      result = result.filter(
-        (club) =>
-          club.name.toLowerCase().includes(term) ||
-          club.category.toLowerCase().includes(term) ||
-          club.tag.toLowerCase().includes(term)
-      );
-    }
-
-    return result;
-  };
+  const { handleFilter, handleSearch, getFilteredAndSearchedData } =
+    useFilters(initialData);
 
   const filteredAndSearchedData = getFilteredAndSearchedData();
 
---
+++ src/app/_hooks/useFilters.ts
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import { Club, Filters, FilterType } from '@/types/club';
+
+const useFilters = (initialData: Club[]) => {
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [filters, setFilters] = useState<Filters>({
+    category: [],
+    campus: '',
+    tag: [],
+  });
+
+  const handleSearch = (value: string) => {
+    setSearchTerm(value);
+  };
+
+  const handleFilter = (filterType: FilterType, value: string) => {
+    let newFilters;
+
+    if (filterType === 'campus') {
+      newFilters = {
+        ...filters,
+        campus: value,
+      };
+    } else {
+      if (value) {
+        newFilters = {
+          ...filters,
+          [filterType]: filters[filterType].includes(value)
+            ? filters[filterType].filter((item) => item !== value)
+            : [...filters[filterType], value],
+        };
+      } else {
+        newFilters = {
+          ...filters,
+          [filterType]: [],
+        };
+      }
+    }
+
+    setFilters(newFilters);
+  };
+
+  const getFilteredAndSearchedData = () => {
+    let result = initialData;
+
+    // 필터 적용
+    if (filters.category.length > 0) {
+      result = result.filter((club) =>
+        filters.category.includes(club.category)
+      );
+    }
+
+    if (filters.campus) {
+      result = result.filter((club) => club.campus === filters.campus);
+    }
+
+    if (filters.tag.length > 0) {
+      result = result.filter((club) => filters.tag.includes(club.tag));
+    }
+
+    if (searchTerm.trim()) {
+      const term = searchTerm.trim().toLowerCase();
+      result = result.filter(
+        (club) =>
+          club.name.toLowerCase().includes(term) ||
+          club.category.toLowerCase().includes(term) ||
+          club.tag.toLowerCase().includes(term)
+      );
+    }
+
+    return result;
+  };
+
+  return { handleFilter, handleSearch, getFilteredAndSearchedData };
+};
+export default useFilters;
