75568f6609eaa27725142b4bc9073d9e44ce8413
leeleeleeleejun <wnstjr6293@naver.com>
2025-03-18 20:21:57 +0900

    refactor: 푸터 내용 변경

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -4,19 +4,27 @@ import KnuIcon from '@/assets/icons/knu.svg';
 export default function Footer() {
   return (
     <footer className='flex w-full bg-gray-50'>
-      <div className='w-full px-6 py-6'>
-        <div className='flex gap-2 items-center text-xs font-medium text-gray-500 md:text-sm'>
+      <div className='w-full px-6 py-6 text-xs text-gray-500'>
+        <div className='flex gap-1 items-center font-semibold md:text-sm'>
           <Image src={KnuIcon} alt={''} width={10} />
-          like-knu
+          <a
+            target='_blank'
+            href='https://www.notion.so/woopaca/722d2e1180f94eeead36ec09436d4576?pvs=4'
+          >
+            개인정보 처리방침
+          </a>
         </div>
-        <a
-          target='_blank'
-          href='https://www.notion.so/woopaca/722d2e1180f94eeead36ec09436d4576?pvs=4'
-          className='text-xs font-semibold text-gray-500 md:text-sm'
-        >
-          개인정보 처리방침
-        </a>
-        <div className='text-xs text-gray-400 md:text-sm'>
+        <p>
+          본 서비스는
+          <a
+            className='font-semibold'
+            target='_blank'
+            href='https://ddingdong.mju.ac.kr/'
+          > {''}띵동
+          </a>
+          의 기능을 참고하여 개발되었습니다.
+        </p>
+        <div className='text-gray-400'>
           E-mail: jcw001031@gmail.com
         </div>
       </div>
