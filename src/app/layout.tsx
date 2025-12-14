6e821ff287daa63c5bb0bfd62f1e45a0d8d7b643
leeleeleeleejun <wnstjr6293@naver.com>
2025-12-15 03:39:33 +0900

    fix: Update font paths in layout.tsx for correct loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,47 +8,47 @@ import {GoogleAnalytics, GoogleTagManager} from '@next/third-parties/google';
 const pretendard = localFont({
   src: [
     {
-      path: '../public/fonts/Pretendard-Thin-Subset.woff2',
+      path: '../../public/fonts/Pretendard-Thin-Subset.woff2',
       weight: '100',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-ExtraLight-Subset.woff2',
+      path: '../../public/fonts/Pretendard-ExtraLight-Subset.woff2',
       weight: '200',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-Light-Subset.woff2',
+      path: '../../public/fonts/Pretendard-Light-Subset.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-Regular-Subset.woff2',
+      path: '../../public/fonts/Pretendard-Regular-Subset.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-Medium-Subset.woff2',
+      path: '../../public/fonts/Pretendard-Medium-Subset.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-SemiBold-Subset.woff2',
+      path: '../../public/fonts/Pretendard-SemiBold-Subset.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-Bold-Subset.woff2',
+      path: '../../public/fonts/Pretendard-Bold-Subset.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-ExtraBold-Subset.woff2',
+      path: '../../public/fonts/Pretendard-ExtraBold-Subset.woff2',
       weight: '800',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-Black-Subset.woff2',
+      path: '../../public/fonts/Pretendard-Black-Subset.woff2',
       weight: '900',
       style: 'normal',
     },
