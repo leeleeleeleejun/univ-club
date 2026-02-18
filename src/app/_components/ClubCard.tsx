c0db951196c295e3521d6be4303325b7fa068584
leeleeleeleejun <wnstjr6293@naver.com>
2026-02-18 19:53:15 +0900

    feat: ClubCard에 updatedAt 필드 추가 및 레이아웃 개선

--- src/app/_components/ClubCard.tsx
+++ src/app/_components/ClubCard.tsx
@@ -2,27 +2,30 @@ import Link from 'next/link';
 import { Club } from '@/types/club';
 import CampusTag from '@/app/_components/CampusTag';
 
-const ClubCard = ({ id, name, tag, category, campus }: Club) => {
+const ClubCard = ({ id, name, tag, category, campus ,updatedAt}: Club) => {
   return (
     <li key={id} className='border-solid border-b-[1.5px] border-gray-100'>
       <Link
         href={`/club/${id}`}
         prefetch={false}
-        className='flex w-full justify-between items-center py-4 gap-1'
+        className='flex flex-col w-full py-4 gap-1'
       >
-        <div>
-          <div className='font-bold text-xl'>{name}</div>
-          <div className='flex items-center'>
-            <div className={`rounded-lg text-sm font-semibold text-zinc-400`}>
-              {category}
-            </div>
-            <div className='px-1 text-sm font-medium text-gray-300'>|</div>
-            <div className='rounded-lg text-sm font-semibold text-zinc-400'>
-              {tag}
+        <div className='flex w-full justify-between'>
+          <div>
+            <div className='font-bold text-xl'>{name}</div>
+            <div className='flex items-center'>
+              <div className={`rounded-lg text-sm font-semibold text-zinc-400`}>
+                {category}
+              </div>
+              <div className='px-1 text-sm font-medium text-gray-300'>|</div>
+              <div className='rounded-lg text-sm font-semibold text-zinc-400'>
+                {tag}
+              </div>
             </div>
           </div>
+          <CampusTag campus={campus} />
         </div>
-        <CampusTag campus={campus} />
+        <p className='text-right text-sm text-zinc-400'>마지막 업데이트: {updatedAt.slice(0, 10)}</p>
       </Link>
     </li>
   );
